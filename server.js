5a9868d4321d992b5545f328929c3c7af9d35d1e
Loftsmart <andrea.airaghi@loft73.com>
2025-05-27 18:22:47 +0200

    Fix node-fetch issue

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
-const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
